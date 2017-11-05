2f435ddf5472e72117d3f70b0926e62e3cf24b9a
Nick Huynh <nick.huynh@uconn.edu>
2017-11-05 08:07:41 -0500

    Update Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -164,17 +164,153 @@ export default {
                         tags: ["cereal", "milk"],
                         left: {
                             name: "Cereal",
-                            responses: 2
+                            responses: 18
                         },
                         right: {
                             name: "Milk",
-                            responses: 4
+                            responses: 3
+                        },
+                        starred: true
+                    },                     {
+                        id: 1,
+                        bg: "https://thedailyblog.co.nz/wp-content/uploads/2017/02/unnamed-7.jpg",
+                        title: "Is climate change an issue that humans should put effort into solving?",
+                        expires: 12,
+                        tags: ["climate change", "society", "politics"],
+                        left: {
+                            name: "YES",
+                            responses: 21
+                        },
+                        right: {
+                            name: "NO",
+                            responses: 8
+                        },
+                        starred: true
+                    },                     {
+                        id: 2,
+                        bg: "http://www.guinnessworldrecords.com/Images/Michael-Jordan-main_tcm25-15662.jpg",
+                        title: "Can LeBron ever pass Jordan as the GOAT with a 2-4 record in the finals?",
+                        expires: 4,
+                        tags: ["sports", "basketball"],
+                        left: {
+                            name: "YES",
+                            responses: 5
+                        },
+                        right: {
+                            name: "NO",
+                            responses: 6
+                        },
+                        starred: false
+                    },                     {
+                        id: 3,
+                        bg: "https://www.computerhope.com/issues/pictures/mac-vs-pc.jpg",
+                        title: "Macs or Windows?",
+                        expires: 8,
+                        tags: ["technology", "computers"],
+                        left: {
+                            name: "Windows",
+                            responses: 18
+                        },
+                        right: {
+                            name: "Mac",
+                            responses: 11
+                        },
+                        starred: false
+                    },                    {
+                        id: 4,
+                        bg: "https://vignette.wikia.nocookie.net/blogclan-2/images/c/c6/Blackboard.png/revision/latest?cb=20160519224101",
+                        title: "Issues Surrounding Common Core",
+                        expires: 25,
+                        tags: ["education", "society", "children"],
+                        left: {
+                            name: "COMMON CORE IS INEFFECTIVE",
+                            responses: 19
+                        },
+                        right: {
+                            name: "COMMON CORE IS EFFECTIVE",
+                            responses: 14
+                        },
+                        starred: false
+                    },{
+                        id: 5,
+                        bg: "https://i.amz.mshcdn.com/qWsvJEjk_3WX87dN-MKQB41gBVI=/950x534/filters:quality(90)/https%3A%2F%2Fblueprint-api-production.s3.amazonaws.com%2Fuploads%2Fcard%2Fimage%2F498047%2F377da321-b088-4497-8d52-4fad8bbf47ca.jpg",
+                        title: "Does Watching Spongebob Make You Dumber?",
+                        expires: 12,
+                        tags: ["television", "society", "children"],
+                        left: {
+                            name: "YES",
+                            responses: 8
+                        },
+                        right: {
+                            name: "NO",
+                            responses: 17
+                        },
+                        starred: false
+                    }, {
+                        id: 6,
+                        bg: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB85HQ1Git0u1-0AdzvprD2G8dcDFVHrhqLG511EyTYHcTIxLj",
+                        title: "Do Vaccines Cause Autism?",
+                        expires: 17,
+                        tags: ["medicine", "society"],
+                        left: {
+                            name: "NO",
+                            responses: 38
+                        },
+                        right: {
+                            name: "YES",
+                            responses: 3
                         },
                         starred: false
                     }
                 ],
                 popular: [
-
+                  {
+                        id: 6,
+                        bg: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB85HQ1Git0u1-0AdzvprD2G8dcDFVHrhqLG511EyTYHcTIxLj",
+                        title: "Do Vaccines Cause Autism?",
+                        expires: 17,
+                        tags: ["medicine", "society"],
+                        left: {
+                            name: "NO",
+                            responses: 38
+                        },
+                        right: {
+                            name: "YES",
+                            responses: 3
+                        },
+                        starred: false
+                    },
+                    {
+                        id: 4,
+                        bg: "https://vignette.wikia.nocookie.net/blogclan-2/images/c/c6/Blackboard.png/revision/latest?cb=20160519224101",
+                        title: "Issues Surrounding Common Core",
+                        expires: 25,
+                        tags: ["education", "society", "children"],
+                        left: {
+                            name: "COMMON CORE IS INEFFECTIVE",
+                            responses: 19
+                        },
+                        right: {
+                            name: "COMMON CORE IS EFFECTIVE",
+                            responses: 14
+                        },
+                        starred: false
+                    },                {
+                        id: 1,
+                        bg: "https://thedailyblog.co.nz/wp-content/uploads/2017/02/unnamed-7.jpg",
+                        title: "Is climate change an issue that humans should put effort into solving?",
+                        expires: 12,
+                        tags: ["climate change", "society", "politics"],
+                        left: {
+                            name: "YES",
+                            responses: 21
+                        },
+                        right: {
+                            name: "NO",
+                            responses: 8
+                        },
+                        starred: true
+                    }       
                 ],
                 forYou: [
                     {
@@ -191,12 +327,57 @@ export default {
                             name: "Milk",
                             responses: 4
                         },
+                        starred: true
+                    }, 
+                    {
+                        id: 4,
+                        bg: "https://vignette.wikia.nocookie.net/blogclan-2/images/c/c6/Blackboard.png/revision/latest?cb=20160519224101",
+                        title: "Issues Surrounding Common Core",
+                        expires: 25,
+                        tags: ["education", "society", "children"],
+                        left: {
+                            name: "COMMON CORE IS INEFFECTIVE",
+                            responses: 19
+                        },
+                        right: {
+                            name: "COMMON CORE IS EFFECTIVE",
+                            responses: 14
+                        },
                         starred: false
-                    }
-
+                    },                {
+                        id: 1,
+                        bg: "https://thedailyblog.co.nz/wp-content/uploads/2017/02/unnamed-7.jpg",
+                        title: "Is climate change an issue that humans should put effort into solving?",
+                        expires: 12,
+                        tags: ["climate change", "society", "politics"],
+                        left: {
+                            name: "YES",
+                            responses: 21
+                        },
+                        right: {
+                            name: "NO",
+                            responses: 8
+                        },
+                        starred: true
+                    } 
                 ],
                 recentlyBuried: [
-
+                    {
+                        id: 4,
+                        bg: "https://vignette.wikia.nocookie.net/blogclan-2/images/c/c6/Blackboard.png/revision/latest?cb=20160519224101",
+                        title: "Issues with PC Culture",
+                        expires: 25,
+                        tags: ["education", "society"],
+                        left: {
+                            name: "PC Culture IS INEFFECTIVE",
+                            responses: 25
+                        },
+                        right: {
+                            name: "PC Culture IS EFFECTIVE",
+                            responses: 19
+                        },
+                        starred: false
+                    }
                 ],
                 starred: [
                     {
@@ -213,7 +394,22 @@ export default {
                             name: "Milk",
                             responses: 4
                         },
-                        starred: false
+                        starred: true
+                    },                     {
+                        id: 1,
+                        bg: "https://thedailyblog.co.nz/wp-content/uploads/2017/02/unnamed-7.jpg",
+                        title: "Is climate change an issue that humans should put effort into solving?",
+                        expires: 12,
+                        tags: ["climate change", "society", "politics"],
+                        left: {
+                            name: "YES",
+                            responses: 21
+                        },
+                        right: {
+                            name: "NO",
+                            responses: 8
+                        },
+                        starred: true
                     }
 
                 ]
